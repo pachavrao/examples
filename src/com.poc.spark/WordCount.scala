23d91e3481785830d1efbbfe0da301d01c0a2aea
venkat <venkateswara.pacha@happiestminds.com>
2020-01-31 12:11:53 +0530

    corrected

--- src/com.poc.spark/WordCount.scala
+++ src/com.poc.spark/WordCount.scala
@@ -14,8 +14,7 @@ object WordCount {
     val sc = new SparkContext(conf)
 
     var a =10
-     a = 10
-    a = a
+
     val all_lines = sc.textFile("in/word_count.text")
     val words = all_lines.flatMap(line => line.split(" "))
 
